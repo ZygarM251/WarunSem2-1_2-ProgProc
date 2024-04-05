92f2ad53841d3591a47027e75ed7b7acc066194d
ZygarM251 <115568775+ZygarM251@users.noreply.github.com>
2024-04-05 18:33:11 +0200

    Add files via upload

---
+++ Lab1_2_3Zadanie.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	char tablicaTesktowa1[100];
+	char tablicaTesktowa2[100];
+
+	cout << "Wprowadz pierwszy tekst: ";
+	cin.getline(tablicaTesktowa1, 100);
+	cout << "Wprowadz drugi tekst: ";
+	cin.getline(tablicaTesktowa2, 100);
+
+
+	if (strlen(tablicaTesktowa1)>100|| strlen(tablicaTesktowa2) > 100)
+	{
+		cout << "Blad za duzo znakow w tablicach";
+		return 1;
+	}
+	else
+	{
+		for (int i = 0; i < strlen(tablicaTesktowa1); i++)
+		{
+			cout << tablicaTesktowa1[i];
+		}
+
+		for (char i = 'a'; i <= 'z'; i++)
+			cout << i << ' ';
+		cout << endl;
+
+		for (char i = 'A'; i <= 'Z'; i++)
+			cout << i << ' ';
+	}
+	return 0;
+
+}
+
